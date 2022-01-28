6b7f2784fd7d3951f27212a464fbb57e6f871b60
Kevin Unkrich <kevinunkrich@ip-192-168-4-93.us-west-2.compute.internal>
2022-01-28 13:36:15 -0800

    Deploy simple implemented via single docker container

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -6,9 +6,37 @@ import sys
 from .db import db
 from .api import MsiApi
 
+
 # Add all monosi modules to PYTHON_PATH
-path = os.path.abspath('../')
+cur_dir_path = os.path.dirname(os.path.realpath(__file__))
+path = os.path.join('../', cur_dir_path)
 sys.path.append(path)
+
+
+def _build_path():
+    build_path = os.path.join(path, 'ui/build/')
+    if not os.path.exists(build_path):
+        raise Exception("Client UI was not built before attempting to serve via Flask.")
+
+    return build_path
+
+def _serve_ui(path=''):
+    build_path = _build_path()
+    req_path = os.path.join(build_path, path)
+
+    if req_path == build_path or not os.path.exists(req_path):
+        path = "index.html"
+
+    return send_from_directory(build_path, path)
+
+def _init_ui(app):
+    if not app.config['SERVE_UI']:
+        return
+
+    app.static_folder =  os.path.join(_build_path(), "static")
+    app.add_url_rule("/", view_func=_serve_ui)
+    app.add_url_rule("/<path:path>", view_func=_serve_ui)
+
 
 def create_app():
     # Initialize Server
@@ -19,6 +47,8 @@ def create_app():
         'server.config.Config'
     )
     app.config.from_object(app_settings)
+
+    _init_ui(app)
 
     # Initialize DB
     db.init_app(app)
