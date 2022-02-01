192e56fb94d55e3537a0015aeaa4296e9731ee5f
Kevin Unkrich <kevinunkrich@Kevins-MacBook-Pro-2.local>
2022-02-01 15:37:27 -0800

    Removing comments

--- src/server/wsgi.py
+++ src/server/wsgi.py
@@ -1,36 +1,7 @@
 from core.common.events import track_event
 
 from . import create_app
-from .models.datasource import Datasource
-from .models.monitor import Monitor
 
-track_event(None, action="server_start")
+track_event(None, action="server_start", label="")
 app = create_app()
 
-def create_monitor_test():
-	print("call!")
-	from .db import db
-
-	# try:
-	# 	datasource = Datasource(
-	# 		name="Snowflake Example",
-	# 		type="snowflake",
-	# 		configuration='{"user": "IVANQZ", "password": "L4v4G1rl$$$", "account": "HKA29277", "warehouse": "COMPUTE_WH", "database": "SNOWFLAKE_SAMPLE_DATA"}',
-	# 	)
-	# 	datasource.create()
-	# except Exception as e:
-	# 	pass
-	# 	# pass # already created
-
-	# monitor = Monitor(
-	# 	name="Example Monitor",
-	# 	description="Description",
-	# 	enabled=True,
-	# 	datasource="Snowflake Example",
-	# 	type="table",
-	# 	configuration='{"table": "snowflake_sample_data.tpch_sf1000.lineitem", "timestamp_field": "L_COMMITDATE"}',
-	# )
-	# monitor.create()
-
-with app.app_context():
-	create_monitor_test()
